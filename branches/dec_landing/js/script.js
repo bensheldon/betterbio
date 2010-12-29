f341003f5f87da333f22726117c2c2e297d9be5b
mark <mark@49b512c4-bc98-4bf6-a88d-f2ca37f76878>
2010-12-29 16:00:23 +0000

    Got the slider working
    git-svn-id: http://betterbio.npnd.com/svn/betterbio@5 49b512c4-bc98-4bf6-a88d-f2ca37f76878

--- branches/dec_landing/js/script.js
+++ branches/dec_landing/js/script.js
@@ -1,26 +1,33 @@
-/* Author: 
+/* Author: Mark Henderson
 
 */
 
+$(document).ready(function() {
+  //Hiding some elements
+  $('.slidercontent').hide();
+  
+  // Main slider animation 
+  $("#main nav li").find("a").click(function(e) {
+    var boxClass = "." + $(this).attr("class");
+    
+    $(".boxContent").remove();
+    $(".box a:not(" + boxClass +")").animate({'width': "50px", 'height': "400px"}, 500)
+                                        .css("text-indent", "-9999px");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    setTimeout(function() {
+      var showDiv = $(boxClass).attr("href");
+      
+      $(boxClass).animate({'height': "2em", 'width': "810px", "line-height": "2em"}, 500, 'linear', 
+        function() { $(boxClass).css("text-indent", "0px"); $(".boxContent").show(); })
+        .parent().append('<div class="boxContent">xx</div>');
+        
+        $(".boxContent").html($(showDiv).html());
+        
+        if(showDiv === "#quiz") {
+          $(".boxContent dd").hide();
+        }
+    }, 501);
+    
+    return false;
+  });
+});
