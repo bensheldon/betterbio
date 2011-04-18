27f70f36233c3c6b9f3c7d6ceb6588decfe43088
Ben Sheldon <bensheldon@gmail.com>
2011-04-18 15:49:53 -0400

    Added default categories to the custom taxonomies

--- wp-content/themes/betterbio/_lib/taxonomies.php
+++ wp-content/themes/betterbio/_lib/taxonomies.php
@@ -83,7 +83,33 @@ function permalink_taxonomy_target($permalink, $post_id, $leavename) {
 	return $permalink;
 }
 
-
+/**
+ * Define default terms for custom taxonomies in WordPress 3.0.1
+ *
+ * @author    Michael Fields     http://wordpress.mfields.org/
+ * @props     John P. Bloch      http://www.johnpbloch.com/
+ *
+ * @since     2010-09-13
+ * @alter     2010-09-14
+ *
+ * @license   GPLv2
+ */
+function mfields_set_default_object_terms( $post_id, $post ) {
+    if ( 'publish' === $post->post_status ) {
+        $defaults = array(
+            'target' => array( 'general' ),
+            'impact' => array( 'general' ),
+            );
+        $taxonomies = get_object_taxonomies( $post->post_type );
+        foreach ( (array) $taxonomies as $taxonomy ) {
+            $terms = wp_get_post_terms( $post_id, $taxonomy );
+            if ( empty( $terms ) && array_key_exists( $taxonomy, $defaults ) ) {
+                wp_set_object_terms( $post_id, $defaults[$taxonomy], $taxonomy );
+            }
+        }
+    }
+}
+add_action( 'save_post', 'mfields_set_default_object_terms', 100, 2 );
 
 
 /**
